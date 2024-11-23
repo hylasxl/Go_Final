8e644f68ebfaf48762f6d97b96a0beaddb2ad523
Trung Việt <100699293+hylasxl@users.noreply.github.com>
2024-11-24 01:21:40 +0700

    changes main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -70,7 +70,7 @@ func main() {
 	}
 	fmt.Println(pong) // Should print "PONG" if connected successfully
 	fixedIP := ""
-	lis, err := net.Listen("tcp", fixedIP+":50051")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
